5f8c8e4c07495132a189351511c33f7f49863892
mzuluagag <mateo.zu.gi1999@gmail.com>
2018-09-08 01:07:03 -0500

    todo listo

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -10,30 +10,30 @@ shinyServer(function(input, output) {
       }
   )
   
-output$satisfaccion <- renderPrint(
+output$satisfaccion <- renderText(
   if (input$apply2) {
     isolate({
       #var7
-      auxVar7 <- 1
+      auxVar7 <- 4
       if (input$var7 == "Buenas") {
-        auxVar7<- 2
-      }else if(input$var7 == "Regulares"){
         auxVar7<- 3
+      }else if(input$var7 == "Regulares"){
+        auxVar7<- 2
       }else if(input$var7 == "Malas"){
-        auxVar7<- 4
+        auxVar7<- 1
       }
       #var8
-      auxVar8 <- 1
+      auxVar8 <- 6
       if (input$var8 == "Propia, la estan pagando") {
-        auxVar8 <- 2
-      }else if (input$var8 == "En arriendo o subarriendo") {
-        auxVar8 <- 3
-      }else if (input$var8 == "Con permiso del propietario, sin pago alguno (usufructuario)") {
-        auxVar8 <- 4
-      }else if (input$var8 == "Posesion sin titulo (ocupante de hecho)") {
         auxVar8 <- 5
+      }else if (input$var8 == "En arriendo o subarriendo") {
+        auxVar8 <- 4
+      }else if (input$var8 == "Con permiso del propietario, sin pago alguno (usufructuario)") {
+        auxVar8 <- 3
+      }else if (input$var8 == "Posesion sin titulo (ocupante de hecho)") {
+        auxVar8 <- 2
       }else if (input$var8 == "Propiedad colectiva") {
-        auxVar8 <- 6
+        auxVar8 <- 1
       }
       #var10
       auxVar10 <- 1
@@ -47,14 +47,9 @@ output$satisfaccion <- renderPrint(
         auxVar10 <- 5
       }else if(input$var10 == "6. Alto"){
         auxVar10 <- 6
-      }else if(input$var10 == "9 No conoce el estrato o no cuenta con recibo de pago."){
-        auxVar10 <- 9
-      }else if(input$var10 == "0 Recibos sin estrato o el servicio es pirata"){
-        auxVar10 <- 0
       }
 
-      predict(modeloLimpio,
-            c(input$var1,
+     predictor(input$var1,
              input$var2,
              input$var3,
              input$var4,
@@ -63,7 +58,7 @@ output$satisfaccion <- renderPrint(
              auxVar7,
              auxVar8,
              input$var9,
-             auxVar10))
+             auxVar10)
   })
 }
  )
