2aee1709023eb4a2ebe58ecf4aff35d9bc4cbe3a
daniel restrepo <daniel2894@gmail.com>
2018-09-08 18:21:00 -0500

    ui de prediccion

--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -10,33 +10,36 @@ shinyUI(fluidPage(
                                           hr(),
                                           fluidRow(
                                             column(2, offset = 4, selectInput("db",choices = total$DIRECTORIO,label = NULL)),
-                                            column(2, offset = 1, actionButton("apply",label = "Generar grafo"))),
+                                            column(2, offset = 1, actionButton("apply",label = "Generar grafo", class = "btn btn-primary"))),
                                           fluidRow(column(6, plotOutput("grafo"), offset = 3))
                                   ),
-                                 tabPanel("Modelo de prediccion de satisfaccion",
-                                   titlePanel("Ingrese los datos a continuacion para realizar la prediccion de satisfaccion."),hr(),
+                            tabPanel("Modelo de prediccion de satisfaccion",
+                                fluidRow(column(6, h1("Ingrese los datos a continuacion para realizar la prediccion de satisfaccion.", align = "center"), offset = 3)),
+                                    hr(),
+                                column(6, offset = 3, 
                                      verticalLayout(
                                        numericInput("var1",
                                                     label = "Ingrese el nivel de satisfaccion que tiene con sus ingresos actualmente, siendo 10 totalmente satisfecho y 0 totalmente insatisfecho.",
-                                                    max = 10,min = 0,step = 1,value = 10),hr(),
+                                                    max = 10,min = 0,step = 1,value = 10, width = "100%"),
                                        numericInput("var2",
                                                     label = "Ingrese el nivel de satisfaccion que tiene con su salud actualmente, siendo 10 totalmente satisfecho y 0 totalmente insatisfecho.",
-                                                    max = 10,min = 0,step = 1,value = 10),hr(),
+                                                    max = 10,min = 0,step = 1,value = 10, width = "100%"),
                                        numericInput("var3",
                                                     label = "Ingrese el nivel de satisfaccion que tiene con su seguridad actualmente, siendo 10 totalmente satisfecho y 0 totalmente insatisfecho.",
-                                                    max = 10,min = 0,step = 1,value = 10),hr(),
+                                                    max = 10,min = 0,step = 1,value = 10, width = "100%"),
                                        numericInput("var4",
                                                     label = "Ingrese el nivel de satisfaccion que tiene con su trabajo actualmente, siendo 10 totalmente satisfecho y 0 totalmente insatisfecho.",
-                                                    max = 10,min = 0,step = 1,value = 10),hr(),
+                                                    max = 10,min = 0,step = 1,value = 10, width = "100%"),
                                        numericInput("var5",
                                                     label = "Ingrese el nivel de felicidad que sintio el día de ayer, siendo 10 muy feliz y 0 muy infeliz.",
-                                                    max = 10,min = 0,step = 1,value = 10),hr(),
+                                                    max = 10,min = 0,step = 1,value = 10, width = "100%"),
                                        numericInput("var6",
                                                     label = "Ingrese que tanto considera que las cosas que hace en su vida actualmente valen la pena, siendo 10 que vale la pena toltalmente y 0 que no valen la pena.",
-                                                    max = 10,min = 0,step = 1,value = 10),hr(),
+                                                    max = 10,min = 0,step = 1,value = 10, width = "100%"),
                                        selectInput("var7",
                                                    label = "Actualmente las condiciones de vida en su hogar son:",
-                                                   choices = c("Muy buenas","Buenas","Regulares","Malas")),hr(),
+                                                   choices = c("Muy buenas","Buenas","Regulares","Malas"),
+                                                   width = "100%"),
                                        selectInput("var8",
                                                    label = "La vivienda ocupada por este hogar es:",
                                                    choices = c("Propia, totalmente pagada",
@@ -44,10 +47,11 @@ shinyUI(fluidPage(
                                                                "En arriendo o subarriendo",
                                                                "Con permiso del propietario, sin pago alguno (usufructuario)",
                                                                "Posesion sin titulo (ocupante de hecho)",
-                                                               "Propiedad colectiva")),hr(),
+                                                               "Propiedad colectiva"),
+                                                   width = "100%"),
                                        numericInput("var9",
                                                     label = "Ingrese la edad promedio de sus hijos (temporal)",
-                                                    max = 100,min = 0,step = 0.1,value = 10),hr(),
+                                                    max = 100,min = 0,step = 0.1,value = 10, width = "100%"),
                                        selectInput("var10",
                                                    label = "Ingrese el estrato para la tarifa de servicios publicos",
                                                    choices = c("1. Bajo - Bajo",
@@ -56,12 +60,13 @@ shinyUI(fluidPage(
                                                                "4. Medio",
                                                                "5. Medio - Alto",
                                                                "6. Alto"
-                                                   )),hr(),
+                                                   ),
+                                                   width = "100%"),
                                        
                                        
-                                       actionButton("apply2",label = "Generar prediccion"),hr(),hr()
+                                       actionButton("apply2",label = "Generar prediccion", width = "100%", class = "btn btn-primary")
                                      ),
-                                   verbatimTextOutput("satisfaccion")
+                                   verbatimTextOutput("satisfaccion"))
                                    )
                                  )
                   
